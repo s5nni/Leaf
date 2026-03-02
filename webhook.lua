4389fbab3b30bd39f8106d490c0e1cb566c9983a
sunni <musafa909@icloud.com>
2026-03-02 15:08:26 +0300

    Update webhook.lua

--- webhook.lua
+++ webhook.lua
@@ -1,1 +1,18 @@
+local Webhooks = {
+    Jewelry_Store = "aaa",
+    Power_Plant   = "aaa",
+    Crown_Jewel   = "aaa",
+    Cargo_Plane   = "aaa",
+    Museum        = "aaa",
+    Rising_Bank   = "aaa",
+    Crater_Bank   = "aaa",
+    Tomb          = "aaa",
+    Oil_Rig       = "aaa",
+    Train         = "aaa",
+    Airdrop       = "aaa",
+    Mansion       = "aaa",
 
+    Log = "aaa"
+}
+
+return Webhooks
