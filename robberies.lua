4f3ace4daa8c0054ba353f65a8e924f64edddfad
sunni <musafa909@icloud.com>
2026-03-02 15:08:53 +0300

    Update robberies.lua

--- robberies.lua
+++ robberies.lua
@@ -1,1 +1,16 @@
+local Robberies = {
+    Jewelry_Store = true,
+    Power_Plant   = true,
+    Crown_Jewel   = true,
+    Cargo_Plane   = true,
+    Museum        = true,
+    Rising_Bank   = true,
+    Crater_Bank   = true,
+    Tomb          = true,
+    Oil_Rig       = true,
+    Train         = true,
+    Airdrop       = true,
+    Mansion       = true,
+}
 
+return Robberies
