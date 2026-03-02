90c5d2c4de8a58a2c36ee879df10f09fbdf8d30c
sunni <musafa909@icloud.com>
2026-03-02 15:09:34 +0300

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,1 +1,12 @@
+-- whitelist.lua
 
+local Whitelist = {
+    ["716FCC14-6490-418D-8A86-F95C17A8FC7B"] = true,
+}
+
+local function IsWhitelisted()
+    local clientId = game:GetService("RbxAnalyticsService"):GetClientId()
+    return Whitelist[clientId] == true
+end
+
+return IsWhitelisted
