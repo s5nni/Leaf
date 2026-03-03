fc251c017ca248d2381b8bb043da6ca9858b699e
sunni <musafa909@icloud.com>
2026-03-03 03:50:02 +0300

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,6 +1,7 @@
 local allowedIds = {
     "716FCC14-6490-418D-8A86-F95C17A8FC7B",
     "3d9c16d6-c4ce-401e-9236-d9d025334012",
+    "8ff63035-bade-4332-9501-c5220da7cca7",
 }
 
 getgenv().WhitelistCheck = function()
